5dead26ad94ea864233556b11d6c8a5375a8f4b0
Saidur Rahman <saidur2k@hotmail.com>
2017-01-14 22:30:24 +1100

    Redux: Extracting Presentational Components (Todo, TodoList)

--- src/index.js
+++ src/index.js
@@ -40,6 +40,35 @@ const getVisibleTodos = (todos, filter) => {
   }
 }
 
+const Todo = ({onClick, completed, text}) => {
+  return (
+    <li
+      onClick={onClick}
+      style={{textDecoration: completed ? 'line-through' : 'none'}}
+    >
+      {text}
+    </li>
+  )
+}
+
+const TodoList = ({todos, onTodoClick}) => {
+  return (
+    <ul>
+      {
+        todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              {...todo}
+              onClick={() => onTodoClick(todo.id)}
+            />
+          )
+        })
+      }
+    </ul>
+  )
+}
+
 
 class TodoApp extends React.Component {
   render () {
@@ -64,29 +93,13 @@ class TodoApp extends React.Component {
           }>
           Add Todo
         </button>
-        <ul>
-          {
-            visibleTodos.map( (todo) => {
-              return (
-                <li
-                  key={todo.id}
-                  onClick={() => {
-                    store.dispatch({
-                      type: 'TOGGLE_TODO',
-                      id: todo.id
-                    })
-                  }}
-                  style={{
-                      textDecoration:
-                        todo.completed ? 'line-through' : 'none'
-                    }}
-                >
-                  {todo.text}
-                </li>
-              )
-            })
-          }
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id => store.dispatch({
+            type: 'TOGGLE_TODO',
+            id: id
+          })}
+        />
         <p>
           Show:
           { ' '}

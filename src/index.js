8169e98cffc22490a0874e4d2fc0d210cd3b4c41
Saidur Rahman <saidur.rahman@luxgroup.com>
2017-01-16 16:39:12 +1100

    Redux: Persisting the State to the Local Storage

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import { createStore } from 'redux'
 
 import { todoApp } from './reducers/index.js'
 import { TodoApp } from './TodoApp'
+import { loadState , saveState } from './localStorage'
+import throttle from 'lodash/throttle'
 
-const persistedState = /* */
+const persistedState = loadState()
+const store = createStore(todoApp, persistedState)
+store.subscribe( throttle(() => {
+  saveState(store.getState())
+}, 1000))
 
 // Render App output
 ReactDOM.render(
-  <Provider store={createStore(todoApp, persistedState)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('root')
